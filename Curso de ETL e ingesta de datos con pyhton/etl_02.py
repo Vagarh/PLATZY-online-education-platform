fbbaf765b5efbf6075faa5c156c7a69ca3ec3202
Juan felipe Arango <juanfelipearango@Juans-iMac.local>
2025-08-19 20:05:45 -0500

    ajustes

---
+++ Curso de ETL e ingesta de datos con pyhton/etl_02.py
@@ -0,0 +1,7 @@
+## series y dataframes diferencias y usos practicos
+
+import pandas as pd
+
+## crear serie, una serie es una estructura unidimensional similar a un array
+
+serie = pd.Series([10,23,24,56])
